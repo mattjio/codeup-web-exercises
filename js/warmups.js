f1c174d7672c5b34878d15526fd3199c7f65a84a
matthew jio <matthew.p.jio1@gmail.com>
2022-04-22 09:11:55 -0500

    warmups april 22

--- js/warmups.js
+++ js/warmups.js
@@ -1,1 +1,24 @@
+(function () {
+
+
+
+
+"use strict";
+
 console.log("Yay! I completed the warm-up!");
+
+// warmup April 22, 2022
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
+
+})();
