4ffe081decfa0542a7b24884d432a322a65512fb
matthew jio <matthew.p.jio1@gmail.com>
2022-05-31 15:57:21 -0500

    weather

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,18 +1,4 @@
-"use strict"
 
-const URL = "https://api.openweathermap.org/data/2.5/forecast";
 
-$.get(URL, {
-    APPID : OPEN_WEATHER_KEY,
-    q: "San Antonio, US",
-    units: "imperial"
-}).done(function (data) {
-    console.log(data);
-    console.log(data.city.name)
-    console.log(data.list[0].main.temp)
-    console.log(data.list[0].weather[0].description)
-    document.getElementById('description').innerHTML = data.list[0].weather[0].description;
-    document.getElementById('temp').innerHTML = data.list[0].main.temp;
-    document.getElementById('location').innerHTML = data.city.name;
-})
+
 
